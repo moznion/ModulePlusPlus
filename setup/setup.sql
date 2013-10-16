e870bd4e05e48bd977ae80097b5b99400d390e02
moznion <moznion@gmail.com>
2013-10-16 17:15:51 +0900

    Enable case-sensitive.

--- setup/setup.sql
+++ setup/setup.sql
@@ -3,16 +3,16 @@ USE modulePlusPlus;
 
 CREATE TABLE IF NOT EXISTS `users` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_hash`     VARCHAR(30)  NOT NULL,
-    `user_name`     VARCHAR(20)  NOT NULL,
-    `user_icon_url` VARCHAR(300) NOT NULL,
+    `user_hash`     VARCHAR(30)  BINARY NOT NULL,
+    `user_name`     VARCHAR(20)  BINARY NOT NULL,
+    `user_icon_url` VARCHAR(300) BINARY NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE (`user_hash`)
 );
 
 CREATE TABLE IF NOT EXISTS `distributions` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) BINARY NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE (`name`)
 );
